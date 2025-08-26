99543f1cc022edd27c6f491b4070ec7e80d64354
Harry Mason <harry.mason@assure-consulting.co.uk>
2025-08-26 14:32:10 +0100

    added test project

---
+++ VMD.RestApiResponseWrapper.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace VMD.RestApiResponseWrapper.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
