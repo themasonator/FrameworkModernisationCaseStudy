05444ecb1b59b4ba0114b74e6a75c593f79be803
Harry Mason <harry.mason@assure-consulting.co.uk>
2025-08-27 09:08:58 +0100

    rename

---
+++ VMD.RestApiResponseWrapper.Test/FullCoverageTestSuite.cs
@@ -0,0 +1,14 @@
+﻿using VMD.RestApiResponseWrapper.Net;
+
+namespace VMD.RestApiResponseWrapper.Net.Test
+{
+    [TestClass]
+    public sealed class FullCoverageTestSuite
+    {
+        [TestMethod]
+        public void FullCoverage()
+        {
+            
+        }
+    }
+}
